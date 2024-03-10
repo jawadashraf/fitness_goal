239b8079c6cc0b16abef14e2cd44c4a6551e396b
mwaqarkt <mwaqarkt@hotmail.com>
2024-03-10 19:57:25 +0000

    Settings updated

--- resources/views/setting/index.blade.php
+++ resources/views/setting/index.blade.php
@@ -6,7 +6,7 @@
             loadurl = "{{route('layout_page')}}?page={{$page}}";
 
             targ = $this.attr('data-target');
-            
+
             id = this.id || '';
 
             $.post(loadurl,{ '_token': $('meta[name=csrf-token]').attr('content') } ,function(data) {
@@ -34,7 +34,7 @@
         <div class="row">
             <div class="col-md-3">
                 <div class="card">
-                    <div class="card-body"> 
+                    <div class="card-body">
                         <div class="row">
                             <div class="col-sm-12 col-lg-12">
                                 <ul class="nav flex-column nav-pills me-3 tabslink" id="tabs-text" role="tablist">
@@ -50,21 +50,21 @@
                                             <li class="nav-item">
                                                 <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=general-setting" data-target=".paste_here" class="nav-link {{ $page == 'general-setting' ? 'active' : '' }}" data-toggle="tabajax" rel="tooltip"> {{ __('message.general_settings') }}</a>
                                             </li>
-                                            <li class="nav-item">
-                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=mobile-config" data-target=".paste_here" class="nav-link {{$page=='mobile-config'?'active':''}}"  data-toggle="tabajax" rel="tooltip"> {{ __('message.mobile_config') }}</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=mail-setting" data-target=".paste_here" class="nav-link {{ $page == 'mail-setting' ?' active' : '' }}" data-toggle="tabajax" rel="tooltip"> {{ __('message.mail_settings') }}</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=language-setting" data-target=".paste_here" class="nav-link {{ $page == 'language-setting' ? 'active': '' }}"  data-toggle="tabajax" rel="tooltip"> {{ __('message.language_settings') }}</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=payment-setting" data-target=".paste_here" class="nav-link {{ $page == 'payment-setting' ? 'active': '' }}"  data-toggle="tabajax" rel="tooltip"> {{ __('message.payment_settings') }}</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=subscription-setting" data-target=".paste_here" class="nav-link {{ $page == 'subscription-setting' ? 'active': '' }}"  data-toggle="tabajax" rel="tooltip"> {{ __('message.subscription_setting') }}</a>
-                                            </li>
+{{--                                            <li class="nav-item">--}}
+{{--                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=mobile-config" data-target=".paste_here" class="nav-link {{$page=='mobile-config'?'active':''}}"  data-toggle="tabajax" rel="tooltip"> {{ __('message.mobile_config') }}</a>--}}
+{{--                                            </li>--}}
+{{--                                            <li class="nav-item">--}}
+{{--                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=mail-setting" data-target=".paste_here" class="nav-link {{ $page == 'mail-setting' ?' active' : '' }}" data-toggle="tabajax" rel="tooltip"> {{ __('message.mail_settings') }}</a>--}}
+{{--                                            </li>--}}
+{{--                                            <li class="nav-item">--}}
+{{--                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=language-setting" data-target=".paste_here" class="nav-link {{ $page == 'language-setting' ? 'active': '' }}"  data-toggle="tabajax" rel="tooltip"> {{ __('message.language_settings') }}</a>--}}
+{{--                                            </li>--}}
+{{--                                            <li class="nav-item">--}}
+{{--                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=payment-setting" data-target=".paste_here" class="nav-link {{ $page == 'payment-setting' ? 'active': '' }}"  data-toggle="tabajax" rel="tooltip"> {{ __('message.payment_settings') }}</a>--}}
+{{--                                            </li>--}}
+{{--                                            <li class="nav-item">--}}
+{{--                                                <a href="javascript:void(0)" data-href="{{ route('layout_page') }}?page=subscription-setting" data-target=".paste_here" class="nav-link {{ $page == 'subscription-setting' ? 'active': '' }}"  data-toggle="tabajax" rel="tooltip"> {{ __('message.subscription_setting') }}</a>--}}
+{{--                                            </li>--}}
                                         @endhasanyrole
                                     @endif
                                 </ul>
