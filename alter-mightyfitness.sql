eb5f253b761fd39e69af480d2ace52b22fb210c6
mwaqarkt <mwaqarkt@hotmail.com>
2024-03-10 12:45:23 +0000

    First Commit

---
+++ alter-mightyfitness.sql
@@ -0,0 +1,2 @@
+-- 24-02-2024
+INSERT INTO `permissions` (`id`, `name`, `title`, `guard_name`, `parent_id`, `created_at`, `updated_at`) VALUES (NULL, 'subscription-add', 'subscription Add', 'web', '90', '2024-02-26 01:42:52', '2024-02-26 01:42:52');
